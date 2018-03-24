1ba95b6fa86b60ce75fbf013754ed67abdb7757b
Joseph Parker <jparker010190@gmail.com>
2018-03-24 02:23:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
  if phrase == "#{phrase}".downcase
-   "HUH?! SPEAK UP, SONNY!"
+   "NO, NOT SINCE 1938!"
  elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO, NOT SINCE 1938!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
 
